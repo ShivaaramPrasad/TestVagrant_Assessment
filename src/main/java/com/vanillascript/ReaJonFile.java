cca34ba8c300c6f332b4460aacf25519ec8b66d9
ShivaaramPrasad <shivaa.ram25@gmail.com>
2023-01-14 16:02:34 +0530

    update code for json array

--- src/main/java/com/vanillascript/ReaJonFile.java
+++ src/main/java/com/vanillascript/ReaJonFile.java
@@ -11,6 +11,8 @@ import org.json.simple.parser.JSONParser;
 public class ReaJonFile {
 
 	public static void main(String[] args) {
+		int wicketKeeperCount=0;
+		int foreignPlayerCount = 0;
 		JSONParser jsonparser = new JSONParser();
 		try {
 			FileReader reader = new FileReader("./src/test/resources/testdata/TeamRCB.json");
@@ -22,6 +24,22 @@ public class ReaJonFile {
 			System.out.println("Team Name: " + teamName);
 			System.out.println("Location: " + location);
 			System.out.println("Players Information");
+			for (int i = 0; i < array.size(); i++) {
+				JSONObject player = (JSONObject)array.get(i);
+				String playersName = (String)player.get("name");
+				String playersCountry = (String)player.get("country");
+				String playersRole = (String)player.get("role");
+				String playersPrice = (String)player.get("price-in-crores");
+				System.out.println("Player Name: "+playersName);
+				System.out.println("Player Country: "+playersCountry);
+				System.out.println("Player Role: "+playersRole);
+				System.out.println("Player Price: "+playersPrice);
+				if(!playersCountry.equals("India")) foreignPlayerCount++;
+				if(playersRole.equals("Wicket-keeper")) wicketKeeperCount++;
+			}	
+			System.out.println("Number of Foreign Player: "+foreignPlayerCount);
+			System.out.println("Number of Wicket Keeper: "+wicketKeeperCount);
+
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
