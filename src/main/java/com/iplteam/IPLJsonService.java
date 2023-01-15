2babd01cdd5c7fc04396f62449d7179e6bfd85cf
ShivaaramPrasad <shivaa.ram25@gmail.com>
2023-01-15 10:41:47 +0530

    updated documentation

--- src/main/java/com/iplteam/IPLJsonService.java
+++ src/main/java/com/iplteam/IPLJsonService.java
@@ -9,6 +9,7 @@ public interface IPLJsonService {
 	/**
 	 * This method used to read the JSON file 
 	 * @param fileName - helps to load the team details`
+	 * @return type void 
 	 * @author Shivaaram
 	 */	
 	public void readJsonFile(String fileName);
@@ -16,6 +17,7 @@ public interface IPLJsonService {
 	/**
 	 * This method used to check how many foreign player in a team 
 	 * @param foreignCount - helps to check the condition number of foreign player in a team
+	 * @return type boolean  
 	 * @author Shivaaram
 	 */
 	public boolean checkForeignPlayers(int foreignCount);
@@ -23,6 +25,7 @@ public interface IPLJsonService {
 	/**
 	 * This method used to check how many wicket keeper in a team 
 	 * @param foreignCount - helps to check the condition number of wicket keeper in a team
+	 * @return type boolean
 	 * @author Shivaaram
 	 */
 	public boolean checkWicketKeeperPlayers(int wicketKeeperCount);
--- src/test/java/ipl/testcase/ValidateNegativeTestCase.java
+++ src/test/java/ipl/testcase/ValidateNegativeTestCase.java
@@ -28,3 +28,4 @@ public class ValidateNegativeTestCase extends IPLJsonServiceImpl {
 	}
 	
 }
+ 
